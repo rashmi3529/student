a6c87760f2d882dc6cccedf97f93d28d67bcfb4b
Rashmi parmar <rashmi9893079175@gmail.com>
2021-04-15 17:32:36 +0530

    student

---
+++ Add.jsp
@@ -0,0 +1,6 @@
+<jsp:useBean id="p" class="pp.Product"></jsp:useBean>
+<jsp:setProperty property="*" name="p"/>
+<jsp:getProperty property="name" name="p"/>
+<jsp:getProperty property="price" name="p"/>
+<jsp:getProperty property="cat" name="p"/>
+<jsp:getProperty property="com" name="p"/>
---
+++ src/Product.java
@@ -0,0 +1,34 @@
+package pp;
+
+public class Product {
+	private String name;
+	private int price;
+	private String cat;
+	private String com;
+	public String getName(){
+	return name;
+
+}
+	public void setName(String name){
+		this.name=name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getCat() {
+		return cat;
+	}
+	public void setCat(String cat) {
+		this.cat = cat;
+	}
+	public String getCom() {
+		return com;
+	}
+	public void setCom(String com) {
+		this.com = com;
+	}
+}
+	
